35c2114781b3cfa3beb43a51ce5740b8554b102e
Nicolas Snider <nicolas.snider@outlook.com>
2022-12-08 20:48:15 -0300

    tests

---
+++ MoviesAPITEst/BaseTest.cs
@@ -0,0 +1,33 @@
+﻿using MoviesAPI;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetTopologySuite;
+using MoviesAPI.Helpers;
+
+namespace MoviesAPITEst
+{
+    internal class BaseTest
+    {
+        protected ApplicationDbContext BuildContext(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(dbName).Options;
+            var dbContext = new ApplicationDbContext(options);
+            return dbContext;
+        }
+        protected IMapper ConfigureAutoMapper()
+        {
+            var config = new MapperConfiguration(opts =>
+            {
+                var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+                opts.AddProfile(new AutomapperProfiles(geometryFactory));
+            });
+            return config.CreateMapper();
+        }
+    }
+}
---
+++ MoviesAPITEst/UnitTest/GenreControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using MoviesAPI.Controllers;
+using MoviesAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesAPITEst.UnitTest
+{
+    [TestClass]
+    internal class GenreControllerTest:BaseTest
+    {
+        [TestMethod]
+        public async Task GetAllGenres()
+        {
+            //prep
+            var nameDb = Guid.NewGuid().ToString();
+            var context = BuildContext(nameDb);
+            var mapper = ConfigureAutoMapper();
+
+            context.Genres.Add(new Genre() { Name = "Genre 1" });
+            context.Genres.Add(new Genre() { Name = "Genre 2" });
+            await context.SaveChangesAsync();
+
+            var context2 = BuildContext(nameDb);
+
+            //test
+            var controller = new GenresController(context2, mapper);
+            var response = await controller.Get();
+            //verification
+            var genres = response.Value;
+            Assert.AreEqual(2, genres.Count);
+        }
+    }
+}
